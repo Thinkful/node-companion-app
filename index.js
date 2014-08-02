d7fb05d49a93d40cded2a5735392cf6feb93daf2
Jeff Escalante <hello@jenius.me>
2014-08-02 19:52:22 -0400

    set up assets

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var path = require('path');
 var port = 3000;
 var app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'assets')))
 
 app.get('/', function(req, res){
   res.render('index');
