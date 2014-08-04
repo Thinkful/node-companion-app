ca7dc3a1ae5dc3552192d88823615a7a237ff753
Jeff Escalante <hello@jenius.me>
2014-08-04 00:27:58 -0400

    add note to js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
 
+  // This is one of those times that you start to see that using a front end MV*
+  // system like backbone, angular, etc might help things. If you had a larger
+  // and more complex app, this type of code can get very messy, very fast.
+
   $('#index .delete').on('click', function(){
     console.log($(this).data('id'));
 
