c9e3707fd1633db57b85432ec17a894097d56c34
Jeff Escalante <hello@jenius.me>
2014-08-03 23:52:36 -0400

    add new items

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose')
 var port = 3000;
 var app = express();
 
+//
 // models
+//
+
 var Item = require('./models/item');
 
+//
 // configuration
+//
+
 mongoose.connect('mongodb://localhost/inventory-dev');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'assets')))
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//
 // routes
+//
+
 app.get('/', function(req, res){
   Item.find(function(err, items){
     if (err) return res.send(500, err);
@@ -20,7 +31,23 @@ app.get('/', function(req, res){
   });
 });
 
-// app.post('/', create)
+app.get('/new', function(req, res){
+  res.render('new');
+});
+
+app.post('/', function(req, res){
+  var item = new Item({
+    name: req.body.name,
+    description: req.body.description
+  });
+
+  item.save(function(err, item){
+    if (err) return res.send(500, err);
+    res.redirect('/');
+  });
+
+});
+
 // app.get('/:id', show)
 // app.put('/:id', update)
 // app.delete('/:id', remove)
